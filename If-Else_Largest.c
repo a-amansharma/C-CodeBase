8cd3f8e0415467df10530befee4442ab1ef824ae
Aman Sharma <rishi-sharma@Amans-MacBook-Pro.local>
2025-02-07 00:16:25 +0530

    Added a file

---
+++ If-Else_Largest.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>b && a>c)
+    {
+        printf("The largest number is : %d",a);
+    }
+    else if(c>b && a<c)
+    {
+        printf("The largest number is : %d",c);
+    }
+    else
+    {
+        printf("The largest number is : %d",b);
+    }
+    return 0;
+}
